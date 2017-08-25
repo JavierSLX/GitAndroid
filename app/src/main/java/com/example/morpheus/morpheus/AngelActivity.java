bc2928475c8d4cdd23461b454efd7076732ea81b
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-08-25 12:35:19 -0500

    comentarios

--- app/src/main/java/com/example/morpheus/morpheus/AngelActivity.java
+++ app/src/main/java/com/example/morpheus/morpheus/AngelActivity.java
@@ -15,6 +15,7 @@ import com.example.morpheus.morpheus.modeloAngel.OpcionAngel;
 import java.util.ArrayList;
 
 public class AngelActivity extends AppCompatActivity {
+    //Declaracion de elementos
     private Spinner spinnerAngel;
     private ListView listviewAngel;
     private ArrayList<OpcionAngel> opcionesAngel;
@@ -23,72 +24,66 @@ public class AngelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_angel);
+        //Se obtiene el array-string deseado
         String []titulos=getResources().getStringArray(R.array.opcionesAngel);
+        //Se inicaliza el arraylist y se le agregan los item del arreglo
         opcionesAngel=new ArrayList<>();
         for(int i=0;i<titulos.length;i++)
         {
             opcionesAngel.add(new OpcionAngel(titulos[i]));
         }
+        //Se crea un nuevo adapter de tipo OpcioAngelAdapter
         OpcionAngelAdapter adapter=new OpcionAngelAdapter(this,opcionesAngel);
+        //Se instancia el spinner del xml con el elemento java y se carga el adapter
         spinnerAngel=(Spinner)findViewById(R.id.spinnerAngel);
         spinnerAngel.setAdapter(adapter);
 
-        //
+        //Metodo que se ejecuta cuando se selecciona un elemento del spinner
         spinnerAngel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
+            //Metodo que devuelve el item seleccionado
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 switch (i)
                 {
+                    //Opcion seleccionada dias
                     case 0:
-                        String []titulosListView1=getResources().getStringArray(R.array.diasAngel);
-                        elementosAngel=new ArrayList<>();
-                        for(int j=0;j<titulosListView1.length;j++)
-                        {
-                            elementosAngel.add(new ItemAngel(titulosListView1[j]));
-                        }
-                        ItemAngelAdapter itemAdapter=new ItemAngelAdapter(AngelActivity.this,elementosAngel);
-                        listviewAngel=(ListView)findViewById(R.id.listaAngel);
-                        listviewAngel.setAdapter(itemAdapter);
+                        String []titulos=getResources().getStringArray(R.array.diasAngel);
+                        mostrarElementos(titulos);
                         break;
+                    //Opcion seleccionada meses
                     case 1:
-                        String []titulosListView2=getResources().getStringArray(R.array.mesesAngel);
-                        elementosAngel=new ArrayList<>();
-                        for(int j=0;j<titulosListView2.length;j++)
-                        {
-                            elementosAngel.add(new ItemAngel(titulosListView2[j]));
-                        }
-                        ItemAngelAdapter itemAdapter2=new ItemAngelAdapter(AngelActivity.this,elementosAngel);
-                        listviewAngel=(ListView)findViewById(R.id.listaAngel);
-                        listviewAngel.setAdapter(itemAdapter2);
+                        titulos=getResources().getStringArray(R.array.mesesAngel);
+                        mostrarElementos(titulos);
                         break;
+                    //Opcion seleccionada equipo
                     case 2:
-                        String []titulosListView3=getResources().getStringArray(R.array.equipoAngel);
-                        elementosAngel=new ArrayList<>();
-                        for(int j=0;j<titulosListView3.length;j++)
-                        {
-                            elementosAngel.add(new ItemAngel(titulosListView3[j]));
-                        }
-                        ItemAngelAdapter itemAdapter3=new ItemAngelAdapter(AngelActivity.this,elementosAngel);
-                        listviewAngel=(ListView)findViewById(R.id.listaAngel);
-                        listviewAngel.setAdapter(itemAdapter3);
+                        titulos=getResources().getStringArray(R.array.equipoAngel);
+                        mostrarElementos(titulos);
                         break;
                 }
 
             }
-
+            //Metodo que se ejecuta cuando no hay elementos seleccionados
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                String []titulosListView2=getResources().getStringArray(R.array.diasAngel);
-                elementosAngel=new ArrayList<>();
-                for(int j=0;j<titulosListView2.length;j++)
-                {
-                    elementosAngel.add(new ItemAngel(titulosListView2[j]));
-                }
-                ItemAngelAdapter itemAdapter2=new ItemAngelAdapter(AngelActivity.this,elementosAngel);
-                listviewAngel=(ListView)findViewById(R.id.listaAngel);
-                listviewAngel.setAdapter(itemAdapter2);
+                String []titulos=getResources().getStringArray(R.array.diasAngel);
+                mostrarElementos(titulos);
             }
         });
     }
+    //Metodo que crea el nuevo arraylist y agrega sus elementos, creo el nuevo adapter de tipo ItemAngelAdapter  lo carga en el
+    //listview
+    public void mostrarElementos(String []titulosListView)
+    {
+        elementosAngel=new ArrayList<>();
+        for(int j=0;j<titulosListView.length;j++)
+        {
+            elementosAngel.add(new ItemAngel(titulosListView[j]));
+        }
+        ItemAngelAdapter itemAdapter2=new ItemAngelAdapter(AngelActivity.this,elementosAngel);
+        listviewAngel=(ListView)findViewById(R.id.listaAngel);
+        listviewAngel.setAdapter(itemAdapter2);
+    }
+
 
 }
