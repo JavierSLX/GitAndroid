fc23a1f6ed9ef0c814abd7016154b4eed43b4e07
SaFabi <ti13110050@itess.edu.mx>
2017-08-25 11:25:09 -0500

    Ya salio

--- app/src/main/java/com/example/morpheus/morpheus/MainActivity.java
+++ app/src/main/java/com/example/morpheus/morpheus/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.morpheus.morpheus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+        private Button btnFabi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnFabi = (Button)findViewById(R.id.Fabi);
+
+        btnFabi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , Fabi.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/morpheus/morpheus/modeloFabi/modelo.java
@@ -0,0 +1,23 @@
+package com.example.morpheus.morpheus.modeloFabi;
+
+/**
+ * Created by Fabi on 25/08/2017.
+ */
+
+public class modelo {
+    private String nombre;
+
+
+    public modelo(String nombre) {
+        this.nombre = nombre;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
